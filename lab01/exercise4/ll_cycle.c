d6774f1b68f6805bb7bc15083e1dd2bf09211dc9
m <197663917+Ali-TaHiR-090@users.noreply.github.com>
2025-06-10 14:27:56 +0500

    do lab01

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,5 +2,21 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+	if (head == NULL) {return 0;}
+    node *fast_p = head;
+	node *slow_p = head;
+
+	do {
+		fast_p = fast_p->next;
+		if (fast_p == NULL) {break;}
+		fast_p = fast_p->next;
+		if (fast_p == NULL) {break;}
+		if (fast_p == slow_p) {break;}
+		slow_p = slow_p->next;
+	} while (fast_p != slow_p || fast_p != NULL);
+	if (fast_p == slow_p) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
